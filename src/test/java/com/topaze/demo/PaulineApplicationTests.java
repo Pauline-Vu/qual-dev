f364aff30979adf6fd9b7f593fac31b198d602f4
Pauline-Vu <paulinevu.pro@gmail.com>
2024-06-06 15:28:52 +0200

    mise à jour

---
+++ src/test/java/com/topaze/demo/PaulineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topaze.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaulineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
